7da386758ee301d32e10b6dbeabceec449d3c2e6
ErikCoelho <erikcoelhomoraes84@gmail.com>
2022-12-20 23:07:52 -0300

    Criando components

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // './components/**/*.{html,js}',
+    // './pages/**/*.{html,js}',
+    './src/**/*.{html,js}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
